cad54c06bc30ba00308cdd59e4a9702adb8983f5
Francisco Percara <frankestudios71@gmail.com>
2022-09-22 13:34:28 -0300

    Saque el error squigles

--- c.cpp
+++ c.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 int main(){
+    //!Constant obviously don't change in the program, and they also named all in caps.
     const double PI = 3.14159;
     double radius = 10;
     double circumference = 2 * PI * radius;
-
     std::cout << circumference << "cm";
-
     return 0;
 }
